c678a2df2a3f57bf733349c6817ff716b86fd548
dalexmon81 <piero.dale81@gmail.com>
2026-08-08 19:15:24 +0200

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
-ciao chatgpt
+
 import gc
 import importlib
 import json
