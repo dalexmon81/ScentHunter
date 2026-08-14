8ae1ec925033945f2205cbb838d4bd25bfb13f37
dalexmon81 <piero.dale81@gmail.com>
2026-08-14 19:37:26 +0200

    Delete backend/test_bplatz_FIX.py

--- backend/test_bplatz_FIX.py
+++
@@ -1,9 +0,0 @@
-from backend.scrapers.bplatz.scraper import search
-
-for query in ("Rasasi Hawas", "Armaf Club de Nuit", "Riiffs"):
-    print("\n" + "=" * 60)
-    print("QUERY:", query)
-    results = search(query)
-    print("RISULTATI:", len(results))
-    for item in results:
-        print(item)
